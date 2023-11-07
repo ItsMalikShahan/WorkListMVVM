6fc854dc4a20d5b275a7b89512bc2927e84cde75
ItsMalikShahan <malikshahannavid@gmail.com>
2023-11-08 03:41:01 +0500

    Add Note activity

---
+++ app/src/main/java/com/example/architectureexample/AddNoteActivity.java
@@ -0,0 +1,68 @@
+package com.example.architectureexample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TITLE ="EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION ="EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY ="EXTRA_PRIORITY  ";
+    EditText title, description;
+    NumberPicker priority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        title = findViewById(R.id.et_title);
+        description = findViewById(R.id.et_description);
+        priority = findViewById(R.id.np_numberPickerPriority);
+
+        priority.setMinValue(0);
+        priority.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_cancel);
+        setTitle("Add Note");
+
+    }
+
+    public void saveNote() {
+        String titleStr = title.getText().toString();
+        String descriptionStr = title.getText().toString();
+        int priorityInt = priority.getValue();
+        if (titleStr.trim().isEmpty() || descriptionStr.trim().isEmpty()) {
+            Toast.makeText(this, "Please provide title and description", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        Intent data = new Intent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.i_save:
+                saveNote();
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+}
